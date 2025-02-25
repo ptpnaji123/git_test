708c4b8127299a24d039d35ca66002bfff612a4f
ptpnaji <ptpnaji123@gmail.com>
2025-02-24 20:30:58 -1200

    Added farewell function

--- app.py
+++ app.py
@@ -1,6 +1,2 @@
-def greet(name):
-    return f"Hello, {name}!"
-
-if __name__ == "__main__":
-    name = input("Enter your name: ")
-    print(greet(name))
+def farewell(name):
+    return f"Goodbye, {name}!"
